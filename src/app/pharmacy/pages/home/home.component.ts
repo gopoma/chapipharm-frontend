6c6f5c6f6e086c51e2eff75a267050d49f4e9f00
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-12-02 16:25:58 -0500

    Prueba con directiva *ngFor

--- src/app/pharmacy/pages/home/home.component.ts
+++ src/app/pharmacy/pages/home/home.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit(): void {
   }
+
+  amigos: number[] = [1,2,3,4,5,6,7,8]
 
 }
